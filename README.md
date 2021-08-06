113758c2dc1e80c7f7f3e76bb7f931d2192c2611
Srijan Das <srijandas18.sd@gmail.com>
2021-08-06 18:14:24 +0530

    improved ui

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # used-car-price-prediction
+![Python](https://img.shields.io/badge/Python-3.8-blueviolet)
+![Framework](https://img.shields.io/badge/Framework-Flask-red)
+![Frontend](https://img.shields.io/badge/Frontend-HTML/CSS/JS-green)
+
+Webapp - https://used-car-price-predic.herokuapp.com/
 
 
-webapp - https://used-car-price-predic.herokuapp.com/
+## How to run the project?
+
+1. Clone or download this repository to your local machine.
+2. Install all the libraries mentioned in the [requirements.txt](https://github.com/SrijanDas/used-car-price-prediction/blob/main/requirements.txt) file with the command `pip install -r requirements.txt`
+4. Open your terminal/command prompt from your project directory and run the file `app.py` by executing the command `python app.py`.
+5. Go to your browser and type `http://127.0.0.1:5000/` in the address bar.
+6. Hurray! That's it.
+
+
+
--- app.py
+++ app.py
@@ -5,14 +5,19 @@ import pickle
 import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
+
 app = Flask(__name__)
-model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
-@app.route('/',methods=['GET'])
+model = pickle.load(open('./model/random_forest_regression_model.pkl', 'rb'))
+
+
+@app.route('/', methods=['GET'])
 def Home():
     return render_template('home.html')
 
 
 standard_to = StandardScaler()
+
+
 @app.route("/predict", methods=['POST'])
 def predict():
     CURRENT_YEAR = 2021
@@ -20,56 +25,55 @@ def predict():
 
     if request.method == 'POST':
         Year = int(request.form['Year'])
-        Present_Price=float(request.form['Present_Price'])
-        Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
-        Owner=int(request.form['Owner'])
-        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
-        Seller_Type_Individual=request.form['Seller_Type_Individual']
-        Transmission_Mannual=request.form['Transmission_Mannual']
-        
+        Present_Price = float(request.form['Present_Price'])
+        Kms_Driven = int(request.form['Kms_Driven'])
+        Kms_Driven2 = np.log(Kms_Driven)
+        Owner = int(request.form['Owner'])
+        Fuel_Type_Petrol = request.form['Fuel_Type_Petrol']
+        Seller_Type_Individual = request.form['Seller_Type_Individual']
+        Transmission_Mannual = request.form['Transmission_Mannual']
+
         car_details = {
-                'year': Year,
-                'price': Present_Price,
-                'kms': Kms_Driven,
-                'owner': Owner,
-                'fuel_type': Fuel_Type_Petrol,
-                'seller': Seller_Type_Individual,
-                'trans': Transmission_Mannual,
-            }
+            'year': Year,
+            'price': Present_Price,
+            'kms': Kms_Driven,
+            'owner': Owner,
+            'fuel_type': Fuel_Type_Petrol,
+            'seller': Seller_Type_Individual,
+            'trans': Transmission_Mannual,
+        }
 
-        if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
+        if (Fuel_Type_Petrol == 'Petrol'):
+            Fuel_Type_Petrol = 1
+            Fuel_Type_Diesel = 0
         else:
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
+            Fuel_Type_Petrol = 0
+            Fuel_Type_Diesel = 1
 
-
-        if(Seller_Type_Individual=='Individual'):
-            Seller_Type_Individual=1
+        if (Seller_Type_Individual == 'Individual'):
+            Seller_Type_Individual = 1
         else:
-            Seller_Type_Individual=0
+            Seller_Type_Individual = 0
 
-        
-        if(Transmission_Mannual=='Mannual'):
-            Transmission_Mannual=1
+        if (Transmission_Mannual == 'Mannual'):
+            Transmission_Mannual = 1
         else:
-            Transmission_Mannual=0
-        
-        
+            Transmission_Mannual = 0
+
         Year = CURRENT_YEAR - Year
 
-        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
-        output=round(prediction[0],2)
-        
-        if output<0:
+        prediction = model.predict([[Present_Price, Kms_Driven2, Owner, Year, Fuel_Type_Diesel, Fuel_Type_Petrol,
+                                     Seller_Type_Individual, Transmission_Mannual]])
+        output = round(prediction[0], 2)
+
+        if output < 0:
             return render_template('home.html', prediction_texts="Sorry, we cannot predict the price.")
         else:
-            return render_template('home.html', prediction_text="Selling Price: {}".format(output), car_details=car_details)
+            return render_template('home.html', prediction_text="Selling Price: {}".format(output),
+                                   car_details=car_details)
     else:
         return render_template('home.html')
 
-if __name__=="__main__":
-    app.run(debug=True)
 
+if __name__ == "__main__":
+    app.run(debug=True)
